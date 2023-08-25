e5d7ec6d5c830fd9085c8f9d03c4c2af3943e4a5
mem848 <meredith.e.modlin@sherwin.com>
2023-08-25 16:20:37 -0400

    stuck on unit testing

--- src/test/java/com/example/onboarding/LaborCalculationTest.java
+++ src/test/java/com/example/onboarding/LaborCalculationTest.java
@@ -1,7 +1,9 @@
 package com.example.onboarding;
 
 import com.example.onboarding.domain.LaborCalculationRequest;
+import com.example.onboarding.domain.LaborCalculationResponse;
 import com.example.onboarding.rest.LaborCalculationController;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,20 +14,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LaborCalculationController.class)
-@WithMockUser(username = "mockuser")
 public class LaborCalculationTest {
     @Autowired
     private MockMvc mvc;
+    @MockBean
+    private LaborCalculationRequest request;
+    @MockBean
+    private LaborCalculationResponse response;
 
     @MockBean
     private LaborCalculationController mockController;
     //https://stackoverflow.com/questions/51346781/how-to-test-post-method-in-spring-boot-using-mockito-and-junit
+    @Test
     public void laborControllerTest () throws Exception
     {
         //for testing, call funcions, see if what is returned is what is expected
         System.out.println("hello world");
-        LaborCalculationRequest myRequest = new LaborCalculationRequest(1,2,3);
-        this.mvc.perform(post("/LaborCalculationController"))
+        this.request = new LaborCalculationRequest(1,2,3);
+        this.response = new LaborCalculationResponse(1234567890, 6);
+        mvc.perform(post("/LaborCalculationController"))
                 //.contentType(MediaType.APPLICATION_JSON_VALUE)
                 //.content(toJson(myRequest))
                 .andExpect(status().isOk());
